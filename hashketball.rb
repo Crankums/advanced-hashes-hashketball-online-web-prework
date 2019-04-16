9a026a5af206fdbeab08bbfab4a96990c85d2cf3
Craig Donavin <crdonavin@gmail.com>
2019-04-16 15:41:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -159,8 +159,8 @@ def team_names
   name_array = []
   game_hash.each do |location, team_data|
     if team_data == :team_name
-      name_array << team_data.key 
-      end 
+      name_array << team_data.key
+      end
   end
   name_array
 end
