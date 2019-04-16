1297a598ac37f241042e710eeb6b18932f979af7
Craig Donavin <crdonavin@gmail.com>
2019-04-16 15:57:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -180,7 +180,7 @@ def player_stats(player_name)
   game_hash.each do |location, team_data|
     team_data[:players].each do |name, stats|
       if name == player_name
-        stat_hash = stats
+        stat_hash = stats.to_i
       end
     end
   end
